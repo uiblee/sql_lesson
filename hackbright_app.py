253043e4a014b640348ad272dbc49246e8eb9fe8
Hackbright Computer <none@none.com>
2012-10-17 20:14:51 -0700

    Sqlite project for Wednesday

--- hackbright_app.py
+++ hackbright_app.py
@@ -13,7 +13,7 @@ Github account: %s"""%(row[0], row[1], row[2])
 
 def connect_to_db():
     global DB, CONN
-    CONN = sqlite3.connect("hackbright.db")
+    CONN = sqlite3.connect("my_database.db")
     DB = CONN.cursor()
 
 def main():
@@ -29,6 +29,29 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "title":
+            query_for_project(*args)
+
+def make_new_student(first_name, last_name, github):
+    query = """INSERT into Students values (?,?,?)"""
+    DB.execute(query, (first_name, last_name, github))
+    CONN.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
+
+
+def query_for_project(title):
+    query = """SELECT description, max_grade, title FROM Projects WHERE title =?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    try: 
+        print row
+        print """\
+        Description: %s  
+        Max Grade: %r
+        Title: %s""" % (row[0], row[1], row[2])
+        #in try-except blocks, you want to put in the error that you want to catch, like so:
+    except TypeError: 
+        print "No row here."
 
     CONN.close()
 
